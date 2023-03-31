822752cd77666ec1d594feae4856cf8f573a4391
Korobka Dmytro <korobka_dmytro@ukr.net>
2023-03-31 18:19:33 +0300

    create demo portfolio

---
+++ js/data.js
@@ -0,0 +1,70 @@
+const mainURL = 'http://localhost/demo/';
+
+const category = [
+    {
+        name: 'Java Script',
+        urlNAme: 'js'
+    },
+    {
+        name: 'React JS',
+        urlNAme: 'react'
+    },
+    {
+        name: 'Node JS',
+        urlNAme: 'node'
+    }
+];
+
+const demos = [
+    {
+        category: 'react',
+        name: 'Moto Store',
+        url: 'http://demo.idvk.info/react/motoshop/',
+        img: 'motoshop.png'
+    },
+    {
+        category: 'react',
+        name: 'Photo Gallery',
+        url: 'http://demo.idvk.info/react/photos/',
+        img: 'photos.png'
+    },
+    {
+        category: 'react',
+        name: 'Money exchange',
+        url: 'http://demo.idvk.info/react/moneychange/',
+        img: 'moneychange.png'
+    },
+    {
+        category: 'react',
+        name: 'Sending invitations',
+        url: 'http://demo.idvk.info/react/users/',
+        img: 'users.png'
+    },
+    {
+        category: 'react',
+        name: 'Modal window',
+        url: 'http://demo.idvk.info/react/modal/',
+        img: 'modal.png'
+    },
+    {
+        category: 'react',
+        name: 'Count',
+        url: 'http://demo.idvk.info/react/count/',
+        img: 'count.png'
+    },
+    {
+        category: 'js',
+        name: 'Alarm Clock',
+        url: 'http://demo.idvk.info/js/alarmclock/',
+        img: 'alarm_clock.png'
+    },
+    {
+        category: 'node',
+        name: 'Node.JS Back End',
+        url: 'https://github.com/korobkadv/NodeJs-Backend',
+        img: 'backend.png'
+    }
+];
+
+
+
